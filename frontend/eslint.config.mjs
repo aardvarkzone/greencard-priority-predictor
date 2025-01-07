97567a08aaee7a573fbd488eb0941f6f47b92f09
Advait Arumugam <advaitarumugam@gmail.com>
2025-01-06 16:01:22 -0800

    working on vercel

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // Allow unused variables for function arguments
+      "@typescript-eslint/no-unused-vars": ["warn", { "args": "none" }],
+    },
+  },
 ];
 
 export default eslintConfig;
